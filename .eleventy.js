9d3504f1c64847da87c5992587057de590c61b3a
Alex Winter <alex@pyxalweb.com>
2022-03-27 18:28:09 -0700

    Organizing template files
    * Split base.njk into multiple includes
    * Added if statement in inc-site-header.njk that will display the site logo as an h1 on the homepage and as a link on any other pages.
    * Created new archive.njk template (which is currently just a copy of home.njk but with a different templateClass.

--- .eleventy.js
+++ .eleventy.js
@@ -119,7 +119,7 @@ module.exports = function(eleventyConfig) {
 
     // These are all optional (defaults are shown):
     dir: {
-      input: "src",
+      input: "_src",
       includes: "_includes",
       data: "_data",
       output: "_site"
